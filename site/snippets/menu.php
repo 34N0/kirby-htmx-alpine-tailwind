03c6b96088e02fac279b4a77447648210ff350fc
34N0 <lex@immerda.ch>
2023-10-18 01:20:58 +0200

    fix: use hx boost for noscript compatibility

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -2,10 +2,8 @@
   <ul>
     <?php foreach($site->children() as $child): ?>
     <li class="list-inside list-disc">
-      <a  hx-get="<?= $child->url() ?>"
-          hx-target="body"
-          hx-swap="innerHTML">
-          <?= $child->title() ?>
+      <a hx-boost="true" href="<?= $child->url() ?>">
+        <?= $child->title() ?>
       </a>
     </li>
     <?php endforeach ?>
