311eb18771f73ba10c9914c62e618743e0eadd0f
34N0 <lex@immerda.ch>
2023-10-18 00:19:13 +0200

    feat: alpinejs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import "./index.css";
 
+
 import Htmx from 'htmx.org';
 window.htmx = Htmx;
+console.log('initialized htmx');
+
+import Alpine from 'alpinejs'
+window.Alpine = Alpine
+Alpine.start()
+console.log('initialized alpinejs');
