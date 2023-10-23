dc672f4ee90df44486f03b22e408b6cd91af59bd
34N0 <lex@immerda.ch>
2023-10-23 02:16:00 +0200

    feat: testing more headers

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -17,16 +17,16 @@
     header('Referrer-Policy: strict-origin-when-cross-origin');
     header('Content-Type: text/html; charset=UTF-8');
     header('Strict-Transport-Security: max-age=31536000; includeSubDomains; preload');
-    header('HTTP Cross-Origin-Opener-Policy: same-origin');
-    /*
     header('Cross-Origin-Embedder-Policy: require-corp');
     header('Cross-Origin-Resource-Policy: same-site');
     header('Permissions-Policy: geolocation=(), camera=(), microphone=()');
     header('Server: webserver');
+    /*
+    header('HTTP Cross-Origin-Opener-Policy: same-origin');
+    */
 
     // csp alpine currently requires unsafe-eval workaround
     header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-eval'; connect-src 'self'; img-src 'self'; style-src 'self'; frame-ancestors 'self'; form-action 'self';")
-  */
     ?>
   
   <noscript>
