89f6e611d882437b9e0b287e64af1919d0f54e31
34N0 <lex@immerda.ch>
2023-10-23 02:25:14 +0200

    fix: COOP Header without HTTP

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -16,14 +16,12 @@
     header('X-Content-Type-Options: nosniff');
     header('Referrer-Policy: strict-origin-when-cross-origin');
     header('Content-Type: text/html; charset=UTF-8');
-    header('Strict-Transport-Security: max-age=31536000; includeSubDomains; preload');
+    header('Strict-Transport-Security: max-age=63072000; includeSubDomains; preload');
     header('Cross-Origin-Embedder-Policy: require-corp');
     header('Cross-Origin-Resource-Policy: same-site');
+    header('Cross-Origin-Opener-Policy: same-origin');
     header('Permissions-Policy: geolocation=(), camera=(), microphone=()');
     header('Server: webserver');
-    /*
-    header('HTTP Cross-Origin-Opener-Policy: same-origin');
-    */
 
     // csp alpine currently requires unsafe-eval workaround
     header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-eval'; connect-src 'self'; img-src 'self'; style-src 'self'; frame-ancestors 'self'; form-action 'self';")
